9d7a4a858fcabe1e9609af90905ffe33c2959979
Kacper Siuda <kacper.siuda@hotmail.com>
2015-12-16 23:19:08 +0100

    I zmiana

--- KlasyAplikacja/KlasyAplikacja/Form1.Designer.cs
+++ KlasyAplikacja/KlasyAplikacja/Form1.Designer.cs
@@ -28,20 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.joeButton = new System.Windows.Forms.Button();
+            this.bobButton = new System.Windows.Forms.Button();
+            this.joeCashLabel = new System.Windows.Forms.Label();
+            this.bobCashLabel = new System.Windows.Forms.Label();
+            this.bankCashLabel = new System.Windows.Forms.Label();
+            this.joeGivesToBob = new System.Windows.Forms.Button();
+            this.bobGivesToJoe = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // joeButton
+            // 
+            this.joeButton.Location = new System.Drawing.Point(27, 158);
+            this.joeButton.Name = "joeButton";
+            this.joeButton.Size = new System.Drawing.Size(75, 60);
+            this.joeButton.TabIndex = 0;
+            this.joeButton.Text = "Daj 10 zł Joemu";
+            this.joeButton.UseVisualStyleBackColor = true;
+            this.joeButton.Click += new System.EventHandler(this.joeButton_Click);
+            // 
+            // bobButton
+            // 
+            this.bobButton.Location = new System.Drawing.Point(108, 158);
+            this.bobButton.Name = "bobButton";
+            this.bobButton.Size = new System.Drawing.Size(75, 60);
+            this.bobButton.TabIndex = 1;
+            this.bobButton.Text = "Weź 5 zł od Boba";
+            this.bobButton.UseVisualStyleBackColor = true;
+            this.bobButton.Click += new System.EventHandler(this.bobButton_Click);
+            // 
+            // joeCashLabel
+            // 
+            this.joeCashLabel.AutoSize = true;
+            this.joeCashLabel.Location = new System.Drawing.Point(24, 39);
+            this.joeCashLabel.Name = "joeCashLabel";
+            this.joeCashLabel.Size = new System.Drawing.Size(35, 13);
+            this.joeCashLabel.TabIndex = 2;
+            this.joeCashLabel.Text = "label1";
+            // 
+            // bobCashLabel
+            // 
+            this.bobCashLabel.AutoSize = true;
+            this.bobCashLabel.Location = new System.Drawing.Point(24, 66);
+            this.bobCashLabel.Name = "bobCashLabel";
+            this.bobCashLabel.Size = new System.Drawing.Size(35, 13);
+            this.bobCashLabel.TabIndex = 3;
+            this.bobCashLabel.Text = "label2";
+            // 
+            // bankCashLabel
+            // 
+            this.bankCashLabel.AutoSize = true;
+            this.bankCashLabel.Location = new System.Drawing.Point(24, 99);
+            this.bankCashLabel.Name = "bankCashLabel";
+            this.bankCashLabel.Size = new System.Drawing.Size(35, 13);
+            this.bankCashLabel.TabIndex = 4;
+            this.bankCashLabel.Text = "label3";
+            // 
+            // joeGivesToBob
+            // 
+            this.joeGivesToBob.Location = new System.Drawing.Point(27, 224);
+            this.joeGivesToBob.Name = "joeGivesToBob";
+            this.joeGivesToBob.Size = new System.Drawing.Size(75, 60);
+            this.joeGivesToBob.TabIndex = 5;
+            this.joeGivesToBob.Text = "Joe daje Bobowi 10 zł";
+            this.joeGivesToBob.UseVisualStyleBackColor = true;
+            this.joeGivesToBob.Click += new System.EventHandler(this.joeGivesToBob_Click);
+            // 
+            // bobGivesToJoe
+            // 
+            this.bobGivesToJoe.Location = new System.Drawing.Point(108, 224);
+            this.bobGivesToJoe.Name = "bobGivesToJoe";
+            this.bobGivesToJoe.Size = new System.Drawing.Size(75, 60);
+            this.bobGivesToJoe.TabIndex = 6;
+            this.bobGivesToJoe.Text = "Bob daje Joemu 5 zł";
+            this.bobGivesToJoe.UseVisualStyleBackColor = true;
+            this.bobGivesToJoe.Click += new System.EventHandler(this.bobGivesToJoe_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(602, 322);
+            this.ClientSize = new System.Drawing.Size(203, 292);
+            this.Controls.Add(this.bobGivesToJoe);
+            this.Controls.Add(this.joeGivesToBob);
+            this.Controls.Add(this.bankCashLabel);
+            this.Controls.Add(this.bobCashLabel);
+            this.Controls.Add(this.joeCashLabel);
+            this.Controls.Add(this.bobButton);
+            this.Controls.Add(this.joeButton);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button joeButton;
+        private System.Windows.Forms.Button bobButton;
+        private System.Windows.Forms.Label joeCashLabel;
+        private System.Windows.Forms.Label bobCashLabel;
+        private System.Windows.Forms.Label bankCashLabel;
+        private System.Windows.Forms.Button joeGivesToBob;
+        private System.Windows.Forms.Button bobGivesToJoe;
     }
 }
 
--- KlasyAplikacja/KlasyAplikacja/Form1.cs
+++ KlasyAplikacja/KlasyAplikacja/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using KlasyAplikacja.Models;
 
 namespace KlasyAplikacja
 {
@@ -15,6 +16,56 @@ namespace KlasyAplikacja
         public Form1()
         {
             InitializeComponent();
+            bob = new Guy();
+            bob.Name = "Bob";
+            bob.Cash = 100;
+
+            joe = new Guy();
+            joe.Name = "Joe";
+            joe.Cash = 50;
+            UpdateForm();
+        }
+
+        private Guy joe;
+        private Guy bob;
+        private int bank = 100;
+
+        public void UpdateForm()
+        {
+            joeCashLabel.Text = joe.Name + " ma " + joe.Cash + " zł";
+            bobCashLabel.Text = bob.Name + " ma " + bob.Cash + " zł";
+            bankCashLabel.Text = "Bank ma " + bank + " zł";
+        }
+
+        private void joeButton_Click(object sender, EventArgs e)
+        {
+            if (bank >= 10)
+            {
+                bank -= joe.ReceiveCash(10);
+                UpdateForm();
+            }
+            else
+            {
+                MessageBox.Show("Bank nie posiada  takiej ilości pieniędzy");
+            }
+        }
+
+        private void bobButton_Click(object sender, EventArgs e)
+        {
+            bank += bob.GiveCash(5);
+            UpdateForm();
+        }
+
+        private void joeGivesToBob_Click(object sender, EventArgs e)
+        {
+            bob.ReceiveCash(joe.GiveCash(10));
+            UpdateForm();
+        }
+
+        private void bobGivesToJoe_Click(object sender, EventArgs e)
+        {
+            joe.ReceiveCash(bob.GiveCash(5));
+            UpdateForm();
         }
     }
 }
