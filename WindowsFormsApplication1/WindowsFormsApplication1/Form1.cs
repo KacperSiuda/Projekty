0d5fbc5931bb6ae4881ef813ab260d513e67cf18
Kacper Siuda <kacper.siuda@hotmail.com>
2015-12-20 16:41:37 +0100

    next Project

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApplication1.Models;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private MenuMaker Menu;
+        public Form1()
+        {
+            InitializeComponent();
+            Menu = new MenuMaker() { Radomizer = new Random() };
+
+        }
+
+        private void UpdateForm()
+        {
+            label1.Text = "Szef kuchni na dzisiaj poleca";
+            label2.Text = Menu.GetMenuItem();
+            label3.Text = Menu.GetMenuItem();
+            label4.Text = Menu.GetMenuItem();
+            label5.Text = Menu.GetMenuItem();
+            label6.Text = "Smacznego :)";
+
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            UpdateForm();
+
+        }
+
+
+
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Models/MenuMaker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1.Models
+{
+    public class MenuMaker
+    {
+        public Random Radomizer;
+
+        public string[] Meats = { "Pieczona wołowina ", "Szynka ", " Salami ", "Indyk ", "Karkówka " };
+        public string[] Condiments =
+        {
+            "żółta musztarda ","brązowa musztarda ","musztarda miodowa ",
+            "majonez ","przyprawa ","sos francuski "
+
+        };
+
+        public string[] Breads =
+        {
+            "chleb ryżowy ","chleb biały ", "chleb zbożowy ",
+            "pumpernikiel ","chleb włoski ","bułka "
+        };
+
+        public string GetMenuItem()
+        {
+            string randomMeat = Meats[Radomizer.Next(Meats.Length)];
+            string randomCondiments = Condiments[Radomizer.Next(Condiments.Length)];
+            string randomBread = Breads[Radomizer.Next(Breads.Length)];
+
+            return randomMeat+"," + randomCondiments+"," + randomBread;
+        }
+    }
+}
